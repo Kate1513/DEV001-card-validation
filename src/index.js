109dc7a037b1a206a91077481d27b85b498256c9
Kate1513 <kate_1513@hotmail.com>
2022-10-04 23:03:08 -0500

    V1 - isValid() y boton funcional .

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import validator from './validator.js';
 
+
+let validButton = document.getElementById('validButton');
+
+validButton.addEventListener('click', validator.isValid);
+
+
 console.log(validator);
